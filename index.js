8d4480ca169256a38676d2b7b401fff560bd326e
Luis Carlos Pineda Rodriguez <95373731+LuisCPineda@users.noreply.github.com>
2024-05-05 15:41:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,20 +32,22 @@ app.post("/miseAJourInterface", (req, res) => {
   const localObjet = req.body.localObjet;
   const isLocalisation = req.body.isLocalisation;
 
-  //   const idNb = req.body.idNb;
-  //   const dateJour = req.body.dateJour;
-  //   const idNbVideoJour = req.body.idNbVideoJour;
-  //   const nbJouer = req.body.nbJouer;
-  //   const tempsTotal = req.body.tempsTotal;
+  const idNb = req.body.idNb;
+  const dateJour = req.body.dateJour;
+  const idNbVideoJour = req.body.idNbVideoJour;
+  const nbJouer = req.body.nbJouer;
+  const tempsTotal = req.body.tempsTotal;
 
   const idVideo = req.body.idVideo;
   const tailleVideo = req.body.tailleVideo;
   const md5Video = req.body.md5Video;
   const ordre = req.body.ordre;
+
   console.log(idObjet);
   const checkTable = pool.query("SELECT * FROM objets WHERE id_objet=?", [
     idObjet,
   ]);
+  console.log(checkTable)
   const idObjetSelected = checkTable.map((row) => row.id_objet);
   if (!idObjetSelected) {
     //insert
