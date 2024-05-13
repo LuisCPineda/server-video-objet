6acc038fe2c3de7782a32b5096d2eab1993a412f
Luis Carlos Pineda Rodriguez <95373731+LuisCPineda@users.noreply.github.com>
2024-05-13 15:58:53 -0400

    Update interface-controller.js

--- api/controller/interface-controller.js
+++ api/controller/interface-controller.js
@@ -28,22 +28,22 @@ export const setInterface = async (req, res) => {
 
     if (videos.length > 0) {
       videos.map(async (video) => {
-        // const reponseVideo = await query(
-        //   "SELECT * from video_objets where id_video=?",
-        //   [video[1]]
-        // );
-        // const idVideoSelected = reponseVideo.map((row) => row.id_video);
+        const reponseVideo = await query(
+          "SELECT * from video_objets where id_video=?",
+          [video[1]]
+        );
+        const idVideoSelected = reponseVideo.map((row) => row.id_video);
         
-        if (!idVideoSelected[0]) {
-          // await query(
-          //   "insert into video_objets (id_objet,id_video) values (?,?)",
-          //   [id_objet, video[1]]
-          // );
-          // await query(
-          //   "insert into nb_video_jour (id_nb,nb_jouer,temps_total,id_objet_nb_video_jour) values (?,?,?,?)",
-          //   [video[0], video[2], video[3], video[1]]
-          // );
-        }
+        // if (!idVideoSelected[0]) {
+        //   await query(
+        //     "insert into video_objets (id_objet,id_video) values (?,?)",
+        //     [id_objet, video[1]]
+        //   );
+        //   await query(
+        //     "insert into nb_video_jour (id_nb,nb_jouer,temps_total,id_objet_nb_video_jour) values (?,?,?,?)",
+        //     [video[0], video[2], video[3], video[1]]
+        //   );
+        // }
         // await query("update video_objets set id_objet=? Where id_video=?", [
         //   id_objet,
         //   video[3],
