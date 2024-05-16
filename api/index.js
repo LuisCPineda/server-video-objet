85d37d9d55c40bcda58e42dc48564ef99eba1e97
Luis Carlos Pineda Rodriguez <95373731+LuisCPineda@users.noreply.github.com>
2024-05-16 10:53:28 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,8 +11,6 @@ dotenv.config();
 
 app.use(express.json());
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use(express.json({ limit: '50mb' }));
