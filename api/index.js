0a27ab4a58a8d38a902bd301448cc77042f6a767
Luis Carlos Pineda Rodriguez <95373731+LuisCPineda@users.noreply.github.com>
2024-05-16 10:52:53 -0400

    fix limit video

--- api/index.js
+++ api/index.js
@@ -11,6 +11,12 @@ dotenv.config();
 
 app.use(express.json());
 
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 console.log(process.env.API_REACT_APP_URL);
 
--- api/middleware/upload-video.js
+++ api/middleware/upload-video.js
@@ -1,11 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: './videos',
+  destination: "./videos",
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
 
-export default upload
+export default upload;
